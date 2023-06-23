293f65998a407687cb0a5ba79d8c7a7dab7e8bfa
mohamed <mohammedbelfares@gmail.com>
2023-06-23 16:49:31 +0100

    nassim 3ta9na wld nass

--- public/js/app.js
+++ public/js/app.js
@@ -140,3 +140,19 @@ dinnerBtn.addEventListener("click", () => {
 
 })
 
+let scroll_up = document.querySelector(".up")
+window.onscroll = function () {
+    if (this.scrollY >= 500) {
+        scroll_up.classList.add("show")
+    } else {
+        scroll_up.classList.remove("show")
+
+    }
+}
+scroll_up.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+})
+
